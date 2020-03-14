17b1837c9cee11df730cd31f293cd0fc7d479396
AlvaradoM <mauriciomj11@gmail.com>
2020-03-14 03:46:52 -0700

    all passed

--- echo.py
+++ echo.py
@@ -1,22 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
 __author__ = "???"
 
-
+import argparse
 import sys
 
 
 def create_parser():
-    """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser("Perform transformation on input text")
+    parser.add_argument('-u', '--upper', action='store_true', help='convert text to uppercase')
+    parser.add_argument('-l', '--lower', action='store_true', help='convert text to lowercase')
+    parser.add_argument('-t', '--title', action='store_true', help='convert text to titlecase')
+    parser.add_argument('text', help="text to be manipulated")
+
+    return parser
 
 
 def main(args):
-    """Implementation of echo"""
-    pass
+    parser = create_parser()
+    ns = parser.parse_args(args)
+    result = ns.text
+    if ns.upper:
+        result = result.upper()
+    if ns.lower:
+        result = result.lower()
+    if ns.title:
+        result = result.title()
+    return result
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -4,9 +4,82 @@
 import unittest
 import echo
 
+
 # Your test case class goes here
+
 class TestEcho(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.parser = echo.create_parser()
+
+    def test_no_options(self):
+        args = ["HeLlo WorLD"]
+        ns = self.parser.parse_args(args)
+        self.assertFalse(ns.upper)
+        self.assertFalse(ns.lower)
+        self.assertFalse(ns.title)
+        actual = echo.main(args)
+        expected = args[0]
+        self.assertEqual(actual, expected)
+
+    def test_all_options(self):
+        args = ['-tul', "HeLlo WorLD"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        self.assertTrue(ns.lower)
+        self.assertTrue(ns.title)
+        actual = echo.main(args)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
+
+    def test_lower_short(self):
+        args = ['-l', "HeLlo world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
+        actual = echo.main(args)
+        expected = "hello world"
+        self.assertEqual(actual, expected)
+
+    def test_upper_short(self):
+        args = ['-u', 'hello world']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        actual = echo.main(args)
+        expected = "HELLO WORLD"
+        self.assertEqual(actual, expected)
+
+    def test_title_short(self):
+        args = ['-t', "HeLlO WoRlD"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
+        actual = echo.main(args)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
+
+    def test_upper_long(self):
+        args = ['--upper', "hello world"]
+        actual = echo.main(args)
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        expected = "HELLO WORLD"
+        self.assertEqual(actual, expected)
+
+    def test_lower_long(self):
+        args = ['--lower', 'Hello world']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
+        actual = echo.main(args)
+        expected = "hello world"
+        self.assertEqual(actual, expected)
+
+    def test_title_long(self):
+        args = ['--title', 'HeLlO world']
+        actual = echo.main(args)
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
